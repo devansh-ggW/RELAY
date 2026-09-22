46f3abd9533950acdbdfed8c2be2921a288ce592
Devansh-S <ryugabuisness@gmail.com>
2026-09-22 17:12:57 +0530

    Mark Google OAuth configuration state explicitly

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
 window.RELAY_CONFIG = {
   supabaseUrl: "https://jeorjtyzstmlpdszwgfn.supabase.co",
-  supabasePublishableKey: "sb_publishable_plT9zzxCB8hBFyFJZ5b-YA_pudu9wY_"
+  supabasePublishableKey: "sb_publishable_plT9zzxCB8hBFyFJZ5b-YA_pudu9wY_",
+  googleOAuthConfigured: false
 };
