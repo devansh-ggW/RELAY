d4e47e1cc015181f2be1db62dc35df1105d6f518
Devansh-S <ryugabuisness@gmail.com>
2026-09-22 17:00:06 +0530

    Connect Relay frontend to Supabase project

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.RELAY_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://jeorjtyzstmlpdszwgfn.supabase.co",
+  supabasePublishableKey: "sb_publishable_plT9zzxCB8hBFyFJZ5b-YA_pudu9wY_"
 };
